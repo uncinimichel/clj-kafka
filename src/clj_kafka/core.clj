42b9c8a2d9b404f7a9b94e38a4f9a07e067b8711
Michel Uncini <uncini.michel@gmail.com>
2017-09-20 20:05:51 +0100

    some file

--- src/clj_kafka/core.clj
+++ src/clj_kafka/core.clj
@@ -1,6 +1,17 @@
-(ns clj-kafka.core)
+(ns clj-kafka.core
+  (:import org.apache.kafka.clients.consumer.ConsumerConfig
+           org.apache.kafka.common.serialization.ByteArraySerializer
+           org.apache.kafka.clients.consumer.KafkaConsumer
+           org.apache.kafka.clients.producer.KafkaProducer
+           org.apache.kafka.clients.producer.ProducerRecord)
+  (:require [taoensso.nippy :as nippy]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+
+(def p-cfg {"value.serializer" ByteArraySerializer
+            "key.serializer" ByteArraySerializer
+            "bootstrap.servers" "localhost:9092"})
+
+(def producer (KafkaProducer. p-cfg))
+
+(.send producer (ProducerRecord. "test" (nippy/freeze "ciao")))
