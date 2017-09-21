d308d277d9edc3f3ae1f3171c4bbe375ae49c9b7
Michel Uncini <uncini.michel@gmail.com>
2017-09-21 20:41:45 +0100

    Added state

--- src/clj_kafka/core.clj
+++ src/clj_kafka/core.clj
@@ -38,3 +38,57 @@
                     (.value)
                     nippy/thaw)]
           (println m))))))
+
+
+
+(defn processing-case-ks
+  "Given an action and a state it produces a new state"
+  [case-s]
+  {:archived (fn []
+               (if (= (:lifecycle-state case-s) :unarchived)
+                 (assoc case-s :lifecycle-state :archived)
+                 case-s))
+   :unarchived (fn []
+                 (if (= (:lifecycle-state case-s) :archived)
+                   (assoc case-s :lifecycle-state :unarchived)
+                   case-s))
+   :deleted (fn []
+              (if (or (= (:lifecycle-state case-s) :archived)
+                      (= (:lifecycle-state case-s) :unarchived))
+                (assoc case-s :lifecycle-state :deleted)
+                case-s))})
+
+                                        ; 1 get a cmd
+                                        ; 2 check if it is a valid command
+                                        ; 3 VALID    --> update state, commit 
+                                        ; 3 NOT-VALID --> send error
+(defn processing-cmds
+  [commands init-state]
+  (loop [state init-state
+         cmds commands
+         good-cmds []
+         bad-cmds []]
+    (let [c (first cmds)]
+      (if (nil? c)
+        {:new-state state
+         :good good-cmds
+         :bad bad-cmds}
+        (let [{:keys [id lifecycle-state]} c
+              old-case-state (get state id)
+              new-case-state ((lifecycle-state (processing-case-ks old-case-state)))]
+          (if (= old-case-state new-case-state)
+            (recur state
+                   (rest cmds)
+                   good-cmds
+                   (conj bad-cmds c))
+            (recur (assoc state id new-case-state)
+                   (rest cmds)
+                   (conj good-cmds c)
+                   bad-cmds)))))))
+
+(def cmds (gen/sample (s/gen :unq/cmd-lifecycle)))
+(def init-state (gen/generate (s/gen :unq/state)))
+(def r (processing-cmds cmds init-state))
+(:state r)
+(:good r)
+(:bad r)
--- src/clj_kafka/specs.clj
+++ src/clj_kafka/specs.clj
@@ -2,10 +2,8 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.spec.gen.alpha :as gen]))
 
-
                                         ; Case spec
-
-(s/def ::id uuid?)
+(s/def ::id #{:111 :222 :333 :444 :555})
 (s/def ::name string?)
 
 (s/def ::creator string?)
@@ -25,76 +23,9 @@
 (s/def ::outstanding-actions boolean?)
 
 (s/def ::case
-  (s/keys :req-un [::id ::creator ::owner ::screening-state ::lifecycle-state ::outstanding-actions]
-          :opt-un [::modifier ::assigned ::last-screened-date ::provider]))
+  (s/keys :req-un [::id ::creator ::owner ::screening-state ::lifecycle-state ::outstanding-actions]))
+                                        ; :opt-un [::modifier ::assigned ::last-screened-date ::provider]))
 
-
-                                        ; State
-                                        ;Comman
 
 (s/def :unq/state (s/map-of ::id ::case))
-
-(s/def :unq/archive-cmd (s/map-of ::id ::case))
-
-(defn is-cmd-valid?
-  [case-s]
-  (println case-s)
-  {:archived (fn []
-               (= (:lifecycle-state case-s) :unarchived))
-   :unarchived (fn []
-                 (= (:lifecycle-state case-s) :archived))
-   :deleted (fn []
-              (or (= (:lifecycle-state case-s) :archived)
-                  (= (:lifecycle-state case-s) :unarchived)))})
-
-
-
-(defn processing-case-ks
-  "Given an action and a state it produces a new state"
-  [case-s]
-  {:archived (fn [{:keys [:lifecycle-state]}])
-   :unarchived (fn [{:keys [:lifecycle-state]}])
-   :deleted (fn[{:keys [:lifecycle-state]}])})
-
-(defn processing-cmds
-  [cmds state]
-  (map (fn [{:keys [id cmd]}]
-         ((cmd (is-cmd-valid? (get state id)))))
-       cmds))
-
-(processing-cmds cmds state)
-
-
-(def cmd1 {:id 1125 :cmd :archived})
-(def cmd2 {:id 1125 :cmd :unarchived})
-(def cmd3 {:id 1125 :cmd :deleted})
-(def cmds [cmd1 cmd2 cmd3])
-
-(def case1
-  {:id 1125
-   :creator "6muau9En0Hgxrg13O2MhSr"
-   :owner "ZW7wHDtj3F"
-   :screening-state :ongoing
-   :lifecycle-state :archived
-   :outstanding-actions true})
-
-(def case2
-  {:id 8888
-   :creator "6muau9En0Hgxrg13O2MhSr"
-   :owner "ZW7wHDtj3F"
-   :screening-state :ongoing
-   :lifecycle-state :deleted
-   :outstanding-actions true})
-
-(def case3
-  {:id 44444
-   :creator "6muau9En0Hgxrg13O2MhSr"
-   :owner "ZW7wHDtj3F"
-   :screening-state :ongoing
-   :lifecycle-state :unarchived
-   :outstanding-actions true})
-
-(def state {1125 case1
-            8888 case2
-            4444 case3
-            })
+(s/def :unq/cmd-lifecycle (s/keys :req-un [::id ::lifecycle-state]))
