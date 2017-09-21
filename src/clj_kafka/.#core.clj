ea0f36df8b549c6fa5633b1421ad5584c9f4aa60
Michel Uncini <michel.uncini@thomsonreuters.com>
2017-09-21 16:55:41 +0100

    new

--- src/clj_kafka/.#core.clj
+++
@@ -1,1 +0,0 @@
-micheluncini@mac-home.mynet.88657
--- src/clj_kafka/core.clj
+++ src/clj_kafka/core.clj
@@ -1,10 +1,12 @@
 (ns clj-kafka.core
   (:import org.apache.kafka.clients.consumer.ConsumerConfig
            org.apache.kafka.common.serialization.ByteArraySerializer
+           org.apache.kafka.common.serialization.ByteArrayDeserializer
            org.apache.kafka.clients.consumer.KafkaConsumer
            org.apache.kafka.clients.producer.KafkaProducer
            org.apache.kafka.clients.producer.ProducerRecord)
-  (:require [taoensso.nippy :as nippy]))
+  (:require [taoensso.nippy :as nippy]
+            [clojure.core.async :as async :refer [go go-loop put! take! <! >! <!! timeout chan alt! go]]))
 
 
 
@@ -15,3 +17,24 @@
 (def producer (KafkaProducer. p-cfg))
 
 (.send producer (ProducerRecord. "test" (nippy/freeze "ciao")))
+
+
+(def c-cfg
+  {"bootstrap.servers" "localhost:9092"
+   "group.id" "avg-rate-consumer"
+   "auto.offset.reset" "earliest"
+   "enable.auto.commit" "false"
+   "key.deserializer" ByteArrayDeserializer
+   "value.deserializer" ByteArrayDeserializer})
+
+(def consumer (doto (KafkaConsumer. c-cfg)
+                (.subscribe ["test"])))
+
+(go
+  (while true
+    (let [records (.poll consumer 100)]
+      (doseq [record records]
+        (let [m (-> record
+                    (.value)
+                    nippy/thaw)]
+          (println m))))))
